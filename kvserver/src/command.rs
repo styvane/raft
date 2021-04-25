8eef46f4c894f0aa3871f5ce96bfc211945a42d2
Styvane Soukossi <mstyvane@gmail.com>
2021-04-25 14:29:09 +0200

    Implement FromStr for Key/Value DT

--- kvserver/src/command.rs
+++ kvserver/src/command.rs
@@ -1,11 +1,30 @@
 /// The `Command` type represents the available database command.
 use serde::Deserialize;
 
+use std::str::FromStr;
+use std::string::ParseError;
+
 #[derive(PartialEq, Eq, Deserialize, Clone, Debug, Ord, PartialOrd, Hash)]
 pub struct Key(String);
 
 #[derive(PartialEq, Eq, Deserialize, Clone, Debug, Ord, PartialOrd)]
 pub struct Value(String);
+
+impl FromStr for Value {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(String::from(s)))
+    }
+}
+
+impl FromStr for Key {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(String::from(s)))
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub enum Command {
